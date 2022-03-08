abec604b26bbc04620817498b9426230de6d8c0a
Ryoma <77398623+ryomahostingph@users.noreply.github.com>
2022-03-08 12:58:41 +0800

    Update recompile_server.sh

--- scripts/recompile_server.sh
+++ scripts/recompile_server.sh
@@ -2,7 +2,7 @@
 zenity --question --title="Ryoma" --text="Are you sure you want to recompile your server? \n\n THIS CAN TAKE UPTO 5 MINUTES TO COMPLETE."
 if [ $? = 0 ]; then
 	cd /home/rathena/Desktop/rAthena
- 	xterm -title "Compile rAthena" -bg black -fg white -e ./configure --enable-packetver=20200401
+ 	xterm -title "Compile rAthena" -bg black -fg white -e ./configure --enable-packetver=20200401 --enable-web-service
 	xterm -title "Compile rAthena" -bg black -fg white -e make clean
 	xterm -title "Compile rAthena" -bg black -fg white -e make server
 	zenity --info --title="Ryoma" --text="Your rAthena server has been recompiled."
